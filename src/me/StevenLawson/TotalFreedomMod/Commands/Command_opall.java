b132bac3b227fca5d7246ce2ae0bf960869fff1d
EthanPlayz <EthanB8094@gmail.com>
2015-01-25 12:20:41 -0600

    Update Command_opall.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_opall.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_opall.java
@@ -7,13 +7,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Op everyone on the server, optionally change everyone's gamemode at the same time.", usage = "/<command> [-c | -s]")
+@CommandParameters(description = "Give everyone OP status", usage = "/<command> [-c | -s]")
 public class Command_opall extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_Util.adminAction(sender.getName(), "Opping all players on the server", false);
+        TFM_Util.adminAction(sender.getName(), "Opping all players!", false);
 
         boolean doSetGamemode = false;
         GameMode targetGamemode = GameMode.CREATIVE;
